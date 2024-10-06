47ba450be9d06559243a37a1ceb18dc0c6f2023f
Ivan Karpiuk <ivankarpiuk@Ivan-MacBook-Pro.local>
2024-10-06 13:47:22 +0400

    build release apk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,6 +19,14 @@ android {
             useSupportLibrary = true
         }
     }
+    signingConfigs {
+        create("release") {
+            storeFile = file("keystore.jks")
+            storePassword = System.getenv("SIGNING_STORE_PASSWORD")
+            keyAlias = System.getenv("SIGNING_KEY_ALIAS")
+            keyPassword = System.getenv("SIGNING_KEY_PASSWORD")
+        }
+    }
 
     buildTypes {
         release {
