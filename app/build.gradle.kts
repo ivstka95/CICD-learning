7fba967da5d92bd55513e0f83f151c0e745dab27
Ivan Karpiuk <ivankarpiuk@Ivan-MacBook-Pro.local>
2024-10-08 09:10:32 +0400

    build release apk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
 
     buildTypes {
         release {
-//            signingConfig = signingConfigs.getByName("release")
+            signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
