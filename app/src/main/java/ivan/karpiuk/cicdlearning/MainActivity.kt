b612baa4ae0829502061167b2615fab81e4592e4
Ivan Karpiuk <ivankarpiuk@Ivan-MacBook-Pro.local>
2024-11-04 14:34:19 +0400

    added caching

--- app/src/main/java/ivan/karpiuk/cicdlearning/MainActivity.kt
+++ app/src/main/java/ivan/karpiuk/cicdlearning/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             CICDLearningTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android 4",
+                        name = "Android 5",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
