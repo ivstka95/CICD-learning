34b0455bf5285e61644ab810b10521a91b507770
Ivan Karpiuk <ivankarpiuk@Ivan-MacBook-Pro.local>
2024-11-10 14:28:31 +0400

    excluded reports

--- app/src/main/java/ivan/karpiuk/cicdlearning/MainActivity.kt
+++ app/src/main/java/ivan/karpiuk/cicdlearning/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import ivan.karpiuk.cicdlearning.ui.theme.CICDLearningTheme
+import ivan.karpiuk.cicdlearning.ui.theme.Colors
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,10 +32,14 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting(
+    name: String,
+    modifier: Modifier = Modifier
+) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier,
+        color = Colors.Pink80
     )
 }
 
