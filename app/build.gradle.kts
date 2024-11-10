ff962e3eed193b78391e5712ad638f20a588aff1
Ivan Karpiuk <ivankarpiuk@Ivan-MacBook-Pro.local>
2024-11-10 09:41:37 +0400

    updated project and dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.compose.compiler)
 }
 
 android {
@@ -57,9 +58,6 @@ android {
     }
     buildFeatures {
         compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
     }
     packaging {
         resources {
