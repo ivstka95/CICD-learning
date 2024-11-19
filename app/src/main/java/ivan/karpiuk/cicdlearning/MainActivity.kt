e60bc799cd079109e9adecfe6db9f2dfae8d255c
Ivan Karpiuk <ivankarpiuk@Ivan-MacBook-Pro.local>
2024-11-19 15:18:11 +0400

    test pass

--- app/src/main/java/ivan/karpiuk/cicdlearning/MainActivity.kt
+++ app/src/main/java/ivan/karpiuk/cicdlearning/MainActivity.kt
@@ -1,11 +1,9 @@
 package ivan.karpiuk.cicdlearning
 
-import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -33,7 +31,6 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@RequiresApi(Build.VERSION_CODES.Q)
 @Composable
 fun Greeting(
     name: String,
