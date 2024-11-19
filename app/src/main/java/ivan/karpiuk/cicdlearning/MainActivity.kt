0ec6820ea57b46a91cd80057eca651174108ec8f
Ivan Karpiuk <ivankarpiuk@Ivan-MacBook-Pro.local>
2024-11-19 13:46:11 +0400

    update

--- app/src/main/java/ivan/karpiuk/cicdlearning/MainActivity.kt
+++ app/src/main/java/ivan/karpiuk/cicdlearning/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
             CICDLearningTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android 13",
+                        name = "Android 14",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
