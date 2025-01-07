fbb19ed4fc4b6aef67232427172eedaf1be6c596
Ivan Karpiuk <ivankarpiuk@Ivan-MacBook-Pro.local>
2025-01-07 10:13:51 +0400

    added dependencyGuard

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.dependency.guard)
 }
 
 android {
@@ -83,4 +84,13 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+}
+
+dependencyGuard {
+    configuration("releaseRuntimeClasspath") {
+        allowedFilter = {
+            // Disallow dependencies with a name containing "junit"
+            !it.contains("junit")
+        }
+    }
 }
